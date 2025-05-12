9dd6b4f9d341dba1329b0a8e2b49a1ad85796bc6
Carlos Rodriguez <carlos.rodriguez@tekus.co>
2025-05-11 20:19:07 -0500

    fix: update base path in vite config and add .gitattributes and .gitignore files

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'feels-technology-test',
+  base: '/engine-project/',
   plugins: [
     vue(),
     vueDevTools(),
